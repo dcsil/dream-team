40fd105fe2aea34f696480a2cbc86d8d8d8da563
Sarthak Saxena <ssarthak.1999@gmail.com>
2020-02-20 19:25:07 -0500

    added sentry integrataion

--- app/django-web-app/dreamtune/dreamtune/settings.py
+++ app/django-web-app/dreamtune/dreamtune/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -98,6 +100,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+#Sentry config
+sentry_sdk.init(
+    dsn="https://43ca244184b54173a2dba326ddf3669b@sentry.io/2721391",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 
 # Internationalization
