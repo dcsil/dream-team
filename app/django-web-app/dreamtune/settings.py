8315fd432a78e4bb43ae79a8a25f7c8a3ad2970b
Sarthak Saxena <ssarthak.1999@gmail.com>
2020-02-20 23:18:10 -0500

    added whitenoise dependency for multithreading

--- app/django-web-app/dreamtune/settings.py
+++ app/django-web-app/dreamtune/settings.py
@@ -16,7 +16,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
