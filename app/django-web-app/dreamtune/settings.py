90ce694ef8d6ad89cd5a4c0f4083f9577618aa79
Sarthak Saxena <ssarthak.1999@gmail.com>
2020-02-20 23:58:23 -0500

    added ip address

--- app/django-web-app/dreamtune/settings.py
+++ app/django-web-app/dreamtune/settings.py
@@ -17,6 +17,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+ALLOWED_HOSTS = ['35.173.27.45', 'localhost', '127.0.0.1']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
