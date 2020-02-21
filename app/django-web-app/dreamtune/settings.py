dfd5c9f382683151421a79cd760c5b9b550a3f0e
Sarthak Saxena <ssarthak.1999@gmail.com>
2020-02-20 23:49:13 -0500

    push to heroku master

--- app/django-web-app/dreamtune/settings.py
+++ app/django-web-app/dreamtune/settings.py
@@ -17,6 +17,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+ALLOWED_HOSTS = 'https://dreamtune.herokuapp.com/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
