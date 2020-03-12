9323bb9c261cd77040dfe5f2b9eaaeb927afd1bf
Sarthak Saxena <ssarthak.1999@gmail.com>
2020-03-12 18:03:48 -0400

    adding static serving

--- app/django-web-app/dream_app/views.py
+++ app/django-web-app/dream_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'public/',
+]
 
 def index(request):
     return HttpResponse("Hello, world. Welcome to Dream-app 1.0.")
