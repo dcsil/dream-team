4971e9545a5ed6ea9b3eac2605ee2998faa99045
Ryan Marten <ryanmarten2000@gmail.com>
2020-03-13 17:03:39 -0400

    changing app test example

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import Login from "./components/Login/Login";
 
-test("renders login button", () => {
+test("renders login button on Home", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
-
