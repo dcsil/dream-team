ae0e8f23827a66268cc7423bb50632c437afdc01
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-03-13 22:02:12 -0400

    dynamically added data to table

--- app/src/components/VenueTable/VenueTable.js
+++ app/src/components/VenueTable/VenueTable.js
@@ -3,34 +3,61 @@ import "./VenueTable.css";
 import data from "../../data/sampleData.json"
 
 class VenueTable extends React.Component {
+    "use strict"
 
     fs = require('fs');
 
-    x() {
-        console.log(data);
+    loadData() {
+        let table = document.getElementById("venues")
+        for(let i = 0; i < data.length; i++){
+            let add = true
+            for (let j = 0; j < table.rows.length; j++){
+                let x = table.rows[j].cells[0].innerHTML
+                if (x === data[i].location){
+                    add = false
+                    let newLeads = parseInt(table.rows[j].cells[1].innerHTML, 10) + 1 
+                    let newEstimate = parseInt(table.rows[j].cells[2].innerHTML, 10) + data[i].estimatedValue
+                    
+                    table.rows[j].cells[1].innerHTML = newLeads.toString()
+                    table.rows[j].cells[2].innerHTML = newEstimate.toString()
+                    break
+                }
+            }
+            if(add){
+                let row = document.createElement("tr")
+                
+                let location = document.createElement("td")
+                location.appendChild(document.createTextNode(data[i].location))
+                
+                let leads = document.createElement("td")
+                leads.appendChild(document.createTextNode("1"))
+                
+                let estimate = document.createElement("td")
+                estimate.appendChild(document.createTextNode(data[i].estimatedValue))
+                
+                row.appendChild(location)
+                row.appendChild(leads)
+                row.appendChild(estimate)
+                
+                table.appendChild(row)
+            }
+        }
     }
 
     componentDidMount() {
-        this.x();
+        this.loadData();
     }
 
     render() {
         return (
             <div>
-                <table>
+                <table id="venues">
 
                     <tr>
                         <th>Location</th>
                         <th>Leads</th>
                         <th>Estimated Value</th>
                     </tr>
-
-                    <tr>
-                        <td>Location</td>
-                        <td>Leads</td>
-                        <td>Estimated Value</td>
-                    </tr>
-
                 </table>
             </div>
         );
