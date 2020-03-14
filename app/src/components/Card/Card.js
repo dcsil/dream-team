ef3207a1fa6d7419a5e382cfddb90775c3d3cb9b
Sarthak Saxena <ssarthak.1999@gmail.com>
2020-03-13 22:14:57 -0400

    removed code smell

--- app/src/components/Card/Card.js
+++ app/src/components/Card/Card.js
@@ -35,21 +35,6 @@ class Home extends React.Component {
   }
 
   componentDidMount() {
-    window.addEventListener("click", clickedButton);
-    console.log("pooasad");
-    function clickedButton(e) {
-      if (
-        e.target.classList.contains("top-right-button") ||
-        (e.target.parentElement &&
-          e.target.parentElement.classList.contains("top-right-button"))
-      ) {
-        if (e.target.textContent.trim() === "Login") {
-          window.location.href = "/login";
-        } else {
-          window.location.href = "/register";
-        }
-      }
-    }
     this.createCards();
   }
 
