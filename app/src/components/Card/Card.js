b887557424d9e3d995f9ed43c1edc94e31e7303f
Sarthak Saxena <ssarthak.1999@gmail.com>
2020-03-13 22:04:35 -0400

    dynamic venues done

--- app/src/components/Card/Card.js
+++ app/src/components/Card/Card.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { createElement } from "react";
 import "./Card.css";
+import venues from "../../data/sampleData.json"
 
 class Home extends React.Component {
+
+  createCards = () => {
+    var infographic = document.createElement("img")
+    infographic.setAttribute("src", "https://img.icons8.com/android/48/000000/info.png");
+    infographic.setAttribute("alt", "https://img.icons8.com/android/48/000000/info.png");
+    var cards_containter = document.getElementById("card_container");
+    for (var venue of venues) {
+      var card = document.createElement("div");
+      card.setAttribute("class", "card");
+
+      var container = document.createElement("div");
+      container.setAttribute("class", "container");
+
+      var name = document.createElement("p");
+      name.setAttribute("class", "venue");
+      name.innerHTML = venue.name;
+      container.append(name);
+      console.log(venue.name);
+
+      var revenue = document.createElement("p");
+      revenue.setAttribute("class", "revenue");
+      revenue.innerHTML = venue.estimatedValue;
+      container.append(revenue);
+
+      container.append(infographic);
+      card.append(container);
+      cards_containter.append(card);
+    }
+  }
+
   componentDidMount() {
     window.addEventListener("click", clickedButton);
-
+    console.log("pooasad");
     function clickedButton(e) {
       if (
         e.target.classList.contains("top-right-button") ||
@@ -18,29 +49,12 @@ class Home extends React.Component {
         }
       }
     }
-  }
-
-  createCard() {
-
+    this.createCards();
   }
 
   render() {
     return (
-      <div class="cardsContainer">
-        <div class="card">
-          <div class="container">
-            <p class="venue">Crab Chack</p>
-            <p class="revenue">$434</p>
-            <img src="https://img.icons8.com/android/48/000000/info.png" />
-          </div>
-        </div>
-        <div class="card">
-          <div class="container">
-            <p class="venue">Crab Chack</p>
-            <p class="revenue">$434</p>
-            <img src="https://img.icons8.com/android/48/000000/info.png" />
-          </div>
-        </div>
+      <div id="card_container" class="cardsContainer">
       </div>
     );
   }
