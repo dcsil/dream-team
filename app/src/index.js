f10f9e80519ad0c9ad2fd51134f04f1658dd02fc
Ryan Marten <ryanmarten2000@gmail.com>
2020-03-15 09:06:28 -0400

    adds sentry error tracking to react app

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import * as Sentry from '@sentry/browser';
+
+// Initialize Sentry: https://docs.sentry.io/platforms/javascript/react
+Sentry.init({ dsn: 'https://6ff9ca5fa0d845f194d15e0d97d7688f@sentry.io/4803343' });
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
