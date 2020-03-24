b101af3589863bb177b4932cf17b3eee240a1d7c
Ryan Marten <ryanmarten2000@gmail.com>
2020-03-24 14:53:51 -0400

    refactors tables again

--- app/src/views/examples/Tables.js
+++ app/src/views/examples/Tables.js
@@ -24,6 +24,9 @@ import Header from "components/Headers/Header.js";
 
 class Tables extends React.Component {
 
+  darkOne = false;
+  darkTwo = true;
+
   profiles = [
     {
       image: require("assets/img/theme/team-1-800x800.jpg"),
@@ -223,116 +226,102 @@ class Tables extends React.Component {
     )
   }
 
+  footer = <CardFooter className="py-4">
+  <nav aria-label="...">
+    <Pagination
+      className="pagination justify-content-end mb-0"
+      listClassName="justify-content-end mb-0"
+    >
+      <PaginationItem className="disabled">
+        <PaginationLink
+          href="#pablo"
+          onClick={e => e.preventDefault()}
+          tabIndex="-1"
+        >
+          <i className="fas fa-angle-left" />
+          <span className="sr-only">Previous</span>
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem className="active">
+        <PaginationLink
+          href="#pablo"
+          onClick={e => e.preventDefault()}
+        >
+          1
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink
+          href="#pablo"
+          onClick={e => e.preventDefault()}
+        >
+          2 <span className="sr-only">(current)</span>
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink
+          href="#pablo"
+          onClick={e => e.preventDefault()}
+        >
+          3
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink
+          href="#pablo"
+          onClick={e => e.preventDefault()}
+        >
+          <i className="fas fa-angle-right" />
+          <span className="sr-only">Next</span>
+        </PaginationLink>
+      </PaginationItem>
+    </Pagination>
+  </nav>
+</CardFooter>
+
+  table(dark, useFooter){
+    return(
+      <div className="col">
+      <Card className={dark ? "bg-default shadow" : "shadow"}>
+        <CardHeader className={dark ? "bg-transparent border-0" : "border-0"}>
+          <h3 className={dark ? "text-white mb-0" : "mb-0"}>Card tables</h3>
+        </CardHeader>
+        <Table
+          className={dark ? "align-items-center table-dark table-flush" : "align-items-center table-flush"}
+          responsive>
+          <thead className={dark ? "thead-dark" : "thead-light"}>
+            <tr>
+              <th scope="col">Project</th>
+              <th scope="col">Budget</th>
+              <th scope="col">Status</th>
+              <th scope="col">Users</th>
+              <th scope="col">Completion</th>
+              <th scope="col" />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.rows.map((row) => this.mappingFunction(row))}
+          </tbody>
+        </Table>
+        {useFooter ? this.footer : <></>}
+      </Card>
+    </div>
+    )
+  }
+
   render() {
     return (
       <>
         <Header />
         {/* Page content */}
         <Container className="mt--7" fluid>
-          {/* Table */}
+          {/* Light Table with Footer */}
           <Row>
-            <div className="col">
-              <Card className="shadow">
-                <CardHeader className="border-0">
-                  <h3 className="mb-0">Card tables</h3>
-                </CardHeader>
-                <Table className="align-items-center table-flush" responsive>
-                  <thead className="thead-light">
-                    <tr>
-                      <th scope="col">Project</th>
-                      <th scope="col">Budget</th>
-                      <th scope="col">Status</th>
-                      <th scope="col">Users</th>
-                      <th scope="col">Completion</th>
-                      <th scope="col" />
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.state.rows.map((row) => this.mappingFunction(row))}
-                  </tbody>
-                </Table>
-                <CardFooter className="py-4">
-                  <nav aria-label="...">
-                    <Pagination
-                      className="pagination justify-content-end mb-0"
-                      listClassName="justify-content-end mb-0"
-                    >
-                      <PaginationItem className="disabled">
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                          tabIndex="-1"
-                        >
-                          <i className="fas fa-angle-left" />
-                          <span className="sr-only">Previous</span>
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem className="active">
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
-                          1
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
-                          2 <span className="sr-only">(current)</span>
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
-                          3
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
-                          <i className="fas fa-angle-right" />
-                          <span className="sr-only">Next</span>
-                        </PaginationLink>
-                      </PaginationItem>
-                    </Pagination>
-                  </nav>
-                </CardFooter>
-              </Card>
-            </div>
+            {this.table(false, true)}
           </Row>
-          {/* Dark table */}
+          {/* Dark table with no Footer */}
           <Row className="mt-5">
-            <div className="col">
-              <Card className="bg-default shadow">
-                <CardHeader className="bg-transparent border-0">
-                  <h3 className="text-white mb-0">Card tables</h3>
-                </CardHeader>
-                <Table
-                  className="align-items-center table-dark table-flush"
-                  responsive
-                >
-                  <thead className="thead-dark">
-                    <tr>
-                      <th scope="col">Project</th>
-                      <th scope="col">Budget</th>
-                      <th scope="col">Status</th>
-                      <th scope="col">Users</th>
-                      <th scope="col">Completion</th>
-                      <th scope="col" />
-                    </tr>
-                  </thead>
-                  <tbody>
-                  {this.state.rows.map((row) => this.mappingFunction(row))}
-                  </tbody>
-                </Table>
-              </Card>
-            </div>
+            {this.table(true, false)}
           </Row>
           </Container>
       </>
