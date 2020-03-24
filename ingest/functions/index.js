de1ea74c43ff78bfeed7bcdd83fa62bf45e35959
Sarthak Saxena <ssarthak.1999@gmail.com>
2020-03-24 09:41:19 -0500

    added basic. Waiting for API key

--- ingest/functions/index.js
+++ ingest/functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
 
+const yelp = require('yelp-fusion');
+const client = yelp.client('YOUR_API_KEY');
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
