6aa8d12ba799370e1c5ab2ae2fe16475877be923
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-03-25 06:27:40 +0530

    testing alias

--- ingest/functions/index.js
+++ ingest/functions/index.js
@@ -1,11 +1,14 @@
 const functions = require('firebase-functions');
-
-// const yelp = require('yelp-fusion');
+var request = require('request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
+const yelp = require('yelp-fusion');
 // const client = yelp.client('YOUR_API_KEY');
+
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+    response.send("Hello from Firebase test 2!");
 });
