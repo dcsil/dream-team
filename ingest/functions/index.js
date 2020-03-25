b4426fa7f09b8e06e455305f5471e942457ad4eb
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-03-25 05:49:09 +0530

    added npm package for yelp

--- ingest/functions/index.js
+++ ingest/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 
-const yelp = require('yelp-fusion');
-const client = yelp.client('YOUR_API_KEY');
+// const yelp = require('yelp-fusion');
+// const client = yelp.client('YOUR_API_KEY');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
