f5317b90d9ba1f156a9134b25222c1905ef53a78
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-03-25 05:41:04 +0530

    firebase setup and initialisation

--- ingest/functions/index.js
+++ ingest/functions/index.js
@@ -3,9 +3,9 @@ const functions = require('firebase-functions');
 const yelp = require('yelp-fusion');
 const client = yelp.client('YOUR_API_KEY');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
