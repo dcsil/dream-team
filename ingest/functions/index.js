8c1202651c9881fa192e9f21d240112af7d7a824
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-03-25 06:53:18 +0530

    adding scraper for bbb and testing it to see if npm module request is working

--- ingest/functions/index.js
+++ ingest/functions/index.js
@@ -9,6 +9,15 @@ var URL = require('url-parse');
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
+function scrapeBBBjs(type) {
+    let response = request.get('https://www.bbb.org/search?find_country=CAN&find_latlng=43.671195%2C-79.394576&find_loc=Toronto%2C%20ON&find_text=' + type + '&page=1');
+    return response;
+};
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase test 2!");
+});
+
+exports.scrapeBBB = functions.https.onRequest((req, res) => {
+    res.send(scrapeBBBjs('restaurant'));
 });
