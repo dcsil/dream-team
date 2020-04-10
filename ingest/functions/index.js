5b36355099fc0653aa48fe6722670be614dc549d
RibhavKapur <ribhav.kapur@mail.utoronto.ca>
2020-04-10 21:50:48 +0530

    second attempt at uploading to database

--- ingest/functions/index.js
+++ ingest/functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cheerio = require('cheerio');
 const rp = require('request-promise');
-const firebase = require("firebase/app");
+const firebase = require("firebase");
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
