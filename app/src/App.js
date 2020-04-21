cee214bc5c365519702b5b586c1ba3df09e8ff0e
Sarthak Saxena <ssarthak.1999@gmail.com>
2020-04-21 01:58:00 -0500

    Final push. pass tests

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
-import Home from "./components/Home/Home";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Auth from "./layouts/Auth"
 
 import "assets/plugins/nucleo/css/nucleo.css";
